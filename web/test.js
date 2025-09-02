1d16c0a7887322191c619ed6c3e8ec83773ce7f6
AlpineVibrations <pro@macbookpro>
2025-09-02 08:52:41 -0700

    test top bar command

--- web/test.js
+++ web/test.js
@@ -2,14 +2,37 @@ import { app } from "../../scripts/app.js";
 
 app.registerExtension({
   name: "aifx",
+   commands: [
+    { 
+      id: "saveAsImage", 
+      label: "Save as Image", 
+      function: () => { 
+        // Code to save canvas as image
+      } 
+    },
+    { 
+      id: "exportWorkflow", 
+      label: "Export Workflow", 
+      function: () => { 
+        // Code to export workflow
+      } 
+    }
+  ],
+  menuCommands: [
+    // Add to File menu
+    { 
+      path: ["File"], 
+      commands: ["saveAsImage", "exportWorkflow"] 
+    }
+  ],
   async setup() {
-    const renderButton = new ComfyButton({
-      icon: "aifx",
-      tooltip: "aifx-render",
-      app,
-      enabled: true,
-      classList: "comfyui-button comfyui-menu-mobile-collapse primary",
-    });
+    // const renderButton = new ComfyButton({
+    //   icon: "aifx",
+    //   tooltip: "aifx-render",
+    //   app,
+    //   enabled: true,
+    //   classList: "comfyui-button comfyui-menu-mobile-collapse primary",
+    // });
     console.log("extension running");
     alert("Setup complete!");
   },
