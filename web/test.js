69ff64af60879b8e69c76e323542df4877d5d69b
AlpineVibrations <pro@macbookpro>
2025-09-02 08:41:24 -0700

    testing

--- web/test.js
+++ web/test.js
@@ -1,17 +1,9 @@
-import { app } from "/scripts/app.js";
+import { app } from "../../scripts/app.js";
 
-app.registerExtension({
-    name: "test",
-    async setup() {
-        console.log("[My Extension] Loaded!");
-
-        // Add a button to the toolbar
-        const toolbar = document.querySelector("#comfy-toolbar");
-        if (toolbar) {
-            const btn = document.createElement("button");
-            btn.innerText = "Click Me!";
-            btn.onclick = () => alert("Hello from My Extension!");
-            toolbar.appendChild(btn);
-        }
-    }
-});
+app.registerExtension({ 
+	name: "aifx",
+	async setup() {
+        console.log("extension running")
+		alert("Setup complete!")
+	},
+})
