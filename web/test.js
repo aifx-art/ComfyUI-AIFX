bda741d4ffd4af6e20aba2a0f433a561e8519e39
AlpineVibrations <pro@macbookpro>
2025-09-02 08:48:20 -0700

    test new button

--- web/test.js
+++ web/test.js
@@ -1,9 +1,16 @@
 import { app } from "../../scripts/app.js";
 
-app.registerExtension({ 
-	name: "aifx",
-	async setup() {
-        console.log("extension running")
-		alert("Setup complete!")
-	},
-})
+app.registerExtension({
+  name: "aifx",
+  async setup() {
+    const renderButton = new ComfyButton({
+      icon: "aifx",
+      tooltip: "aifx-render",
+      app,
+      enabled: true,
+      classList: "comfyui-button comfyui-menu-mobile-collapse primary",
+    });
+    console.log("extension running");
+    alert("Setup complete!");
+  },
+});
